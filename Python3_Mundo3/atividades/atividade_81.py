1a2eb7e61c1ab045f29f0e14d25fe08bed65f855
Diógenes <matheusdiorgines26@gmail.com>
2025-12-24 20:55:52 -0300

    Update Atvd 80

--- Python3_Mundo3/atividades/atividade_81.py
+++ Python3_Mundo3/atividades/atividade_81.py
@@ -2,10 +2,7 @@ valores = []
 
 while True:
     valor = int(input("Digite um valor: "))
-    if valor in valores:
-        print("Valor duplicado! Não vou adicionar.")
-    else:
-        valores.append(valor)
+    valores.append(valor)
 
     opcao = input("Quer continuar? [S/N] ").strip().upper()
     if opcao == 'N':
