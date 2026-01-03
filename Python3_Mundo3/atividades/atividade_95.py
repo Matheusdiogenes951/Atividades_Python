b3cce5c4592251f6d615d535762e7bba48c72a73
Diógenes <matheusdiorgines26@gmail.com>
2026-01-03 17:53:06 -0300

    add atvd 95
    fiz a atvd 95 e seu readme

--- Python3_Mundo3/atividades/atividade_95.py
+++ Python3_Mundo3/atividades/atividade_95.py
@@ -1,21 +1,38 @@
-dados = dict()
+pessoa = dict()
+galera = list()
+soma = media = 0
+while True:
+    pessoa.clear()
+    pessoa['Nome'] = str(input('Nome: '))
+    while True:
+        pessoa['Sexo'] = str(input('Sexo [M/F]: ')).upper().strip()[0]
+        if pessoa['Sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['Idade'] = int(input('Idade: '))
+    soma += pessoa['Idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break    
 
-dados['Nome'] = str(input('Nome do Jogador: '))
-partidas = int(input(f'Quantas partidas {dados["Nome"]} jogou? '))
-gols = list()
-for c in range(0, partidas):
-    gols.append(int(input(f'   Quantos gols na partida {c + 1}? ')))
-dados['Gols'] = gols
-dados['Total'] = sum(gols)
 print('-=' * 30)
-print(dados)
-print('-=' * 30)
-for k, v in dados.items():
-    print(f'O Campo {k} tem o valor {v}')
-print('-=' * 30)
-print(f'O jogador {dados["Nome"]} jogou {partidas} partidas')
-
-for i in range(0, partidas):
-    print(f'   => Na partida {i + 1}, fez {dados["Gols"][i]} gols.')
-print(f'Foi um total de {dados["Total"]} gols.')
-
+print(f'Ao todo temos {len(pessoa)} pessoas cadastradas.')
+media = soma / len(galera)
+print(f'A média de idade é de {media:5.2f} anos.')
+print('As mulheres cadastradas foram: ', end='')
+for p in galera:
+    if p['Sexo'] == 'F':
+        print(f'{p["Nome"]} ', end='')
+print()
+print('Lista de pessoas que estão acima da média: ')
+for p in galera:
+    if p['Idade'] >= media:
+        for k, v in p.items():
+            print(f'    {k} = {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
