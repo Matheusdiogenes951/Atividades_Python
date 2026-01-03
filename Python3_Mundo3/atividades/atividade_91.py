6a86d96a49f3677d4f4cc35a46a85400df557058
Diógenes <matheusdiorgines26@gmail.com>
2026-01-03 15:25:04 -0300

    Add este arquivo

--- Python3_Mundo3/atividades/atividade_91.py
+++ Python3_Mundo3/atividades/atividade_91.py
@@ -1,18 +1,16 @@
-aluno = {'nome': '', 'Media': 0,}
+aluno = {}
 
 nome = str(input("Nome: "))
 Media = float(input('Media: '))
 
-aluno['nome'] = nome
-aluno['Media'] = Media
+if aluno['Media'] >= 7:
+    aluno['Situacao'] = 'Aprovado'
+elif 5 <= aluno['Media'] < 7:
+    aluno['Situacao'] = 'Recuperacao'
+else:
+    aluno['Situacao'] = 'Reprovado'
 
 print('-=' * 30)
 
-print(f'Nome e igual a {aluno["nome"]}]')
-
-print(f'Media e igual a {aluno["Media"]}]')
-
-if aluno['Media'] >= 7:
-    print('Situacao e aprovado')
-else:
-    print('Situacao e reprovado')
+for k, v in aluno.items():
+    print(f' - {k} e igual a {v}')
