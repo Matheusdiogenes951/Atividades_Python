cbdb707f1366b9345b3f131994ad8b4e476c168c
Diógenes <matheusdiorgines26@gmail.com>
2026-01-05 13:53:48 -0300

    add atvd 102
    Fiz a atividade 102 e seu readme

---
+++ Python3_Mundo3/atividades/atividade_102.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+def voto(ano):
+    idade = date.today().year - ano
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+# Programa Principal
+nasc = int(input('Em que ano você nasceu? '))
+print(voto(nasc))
+
