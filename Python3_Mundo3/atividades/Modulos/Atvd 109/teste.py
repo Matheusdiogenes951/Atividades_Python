858d070bcd1396b6f3f104ddf72b86254fae3d57
Diógenes <matheusdiorgines26@gmail.com>
2026-01-08 11:07:29 -0300

    Add atvd 109
    Fiz a atividade 109 e seu readme

---
+++ Python3_Mundo3/atividades/Modulos/Atvd 109/teste.py
@@ -0,0 +1,8 @@
+import Moeda
+
+p = float(input('Digite um preco: '))
+print(f'A metade de {Moeda.moeda(p)} e {Moeda.moeda(Moeda.metade(p))}')
+print(f'O dobro de {Moeda.moeda(p)} e {Moeda.moeda(Moeda.dobro(p))}')
+print(f'Aumentando 10 %, temos {Moeda.moeda(Moeda.aumentar(p, 10))}')
+print(f'Diminuindo 10 %, temos {Moeda.moeda(Moeda.diminuir(p, 10))}')
+
